68019ebe80e1300213976962a980109efec2fc56
cal1k <calvin.taylor@live.com>
2018-10-25 21:40:51 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://dbuser2:dbuser2password@ds227243.mlab.com:27243/firsttestdb';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +15,7 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 
 var app = express();
@@ -24,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
